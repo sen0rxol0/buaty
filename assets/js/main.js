eaab3631e4ad85511cd32b7f26918a33d36e8f3d
sen0rxol0 <sen0rxol0@baieflow.com>
2021-03-16 09:36:02 +0100

    Added initial script, scroll events and menu toggle events

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function initNavigationToggle() {
+    const menuButton = document.querySelector('button[data-menu]');
+    menuButton.addEventListener('click', (ev) => {
+        ev.target.classList.toggle('toggled');
+        document.querySelector('header.header-main').classList.toggle('nav-active');
+        document.querySelector('nav.navigation').classList.toggle('active');
+    });
+}
+
+function onLinkClickScroll(ev) {
+
+}
+
+function revealsOnScroll() {
+    const revealElements = document.querySelectorAll('*[data-reveals]');
+    revealElements.forEach((el, idx) => {
+        const revealsAt = (window.scrollY + window.innerHeight) - (el.scrollHeight / 2);
+        if (revealsAt > el.offsetTop) {
+            el.classList.add('revealed');
+        }
+        if (idx === (revealElements.length - 1) && el.classList.contains('revealed')) {
+            window.removeEventListener('scroll', revealsOnScroll);
+        }
+    });
+}
+
+function initScrollEvents() {
+    window.addEventListener('scroll', revealsOnScroll);
+    let links = document.querySelectorAll('a[href*="#"]');
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', onLinkClickScroll);
+    }
+}
+
+document.onreadystatechange = function onStateChange() {
+    if (document.readyState === 'complete') {
+        initNavigationToggle();
+        initScrollEvents();
+    }
+}
